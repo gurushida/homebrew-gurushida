43e2bccf7175715b34e3abd73be2e43b6ce465bc
gurushida <49831684+gurushida@users.noreply.github.com>
2020-12-10 14:48:44 +0100

    Added cask for 80x25

---
+++ Casks/80x25.rb
@@ -0,0 +1,9 @@
+cask "80x25" do
+  version ":latest"
+  sha256 "137829e91886d6272a71f16cca04451fe916bb714549720eb05b13881c30ca4c"
+  url "https://80x25.fun/80x25.zip"
+  name "80x25"
+  homepage "https://80x25.fun"
+
+  app "80x25"
+end
